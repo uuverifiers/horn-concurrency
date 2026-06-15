cecbb96f8fd687f8d5b1d00d12e994566d8eea92
Gustav Ung <gustav.ung@se.traton.com>
2026-06-15 11:59:58 +0200

    Add new test for MITL model checking

--- src/test/scala/MITLTranslator.scala
+++ src/test/scala/MITLTranslator.scala
@@ -19,7 +19,7 @@ class MITLTranslatorTests extends FlatSpec {
     import Rationals.{geq, minus, int2ring => toRat}
 
     "MITL Once" should "prohibit runs where output is false for five time units after p" in {
-        val formula = PDiamond(OpenOpen(0, 5), AP("p"))
+        val formula = PDiamond(OpenOpen(0, 5), AP("p"), None)
         val encoded = encodeTransducerEquation(mitlTranslation(formula))
 
         val signalByLabel = encoded.head.globalSignalLabels.zip(encoded.head.globalSignalTerms).toMap
@@ -47,12 +47,55 @@ class MITLTranslatorTests extends FlatSpec {
                 NoSync)
         )
 
-        val monProgressBlocks = List(
-            ProgressBlock(List(Clause(HornClauses.FALSE(), List(mon(0)(monArgs: _*)), true))),
-            ProgressBlock(List((qSignal === False) :- mon(1)(monArgs: _*)))
-        )
+        val monProgressBlocks = List()
 
         val assertion = false :- mon(2)(monArgs: _*)
+
+        val globalVarNum = 1 + encoded.head.globalSignalTerms.size
+        val system = SignalSystem(
+            encoded.map(t => (t.clauses, System.Singleton)) :+ (monProc, System.Singleton),
+            globalVarNum,
+            Seq(assertion),
+            RationalTime(0),
+            (1 until globalVarNum).toSet,
+            encoded.map(t => List(t.progressBlock)) :+ monProgressBlocks
+        )
+
+        val encoder = new SignalEncoder(system)
+        val vl = runLoop(encoder.result)
+        assert(isSolvable(vl))
+    }
+
+    "MITL conjunction" should "prohibit runs where output is true in contradiction" in {
+        val formula = Conjunction(AP("p"), Negation(AP("p")))
+        // Faster test
+        // val formula = Negation(Disjunction(Negation(AP("p")), AP("p")))
+        val normalizedFormula = nf(formula)
+        val encoded = encodeTransducerEquation(mitlTranslation(normalizedFormula))
+
+        val signalByLabel = encoded.head.globalSignalLabels.zip(encoded.head.globalSignalTerms).toMap
+        val pSignal = signalByLabel("p")
+        val qSignal = signalByLabel("0")
+
+        val cMon = Rationals.dom.newConstant("c_mon")
+        val monSorts = List(Rationals.dom) ++
+            encoded.head.globalSignalTerms.map(_ => Sort.Bool) ++
+            List(Rationals.dom)
+        val monArgs = Seq(C) ++ encoded.head.globalSignalTerms ++ Seq(cMon)
+        val resetMonArgs = Seq(C) ++ encoded.head.globalSignalTerms ++ Seq(C)
+        val mon = for (i <- 0 to 1) yield MonoSortedPredicate("monitor" + i, monSorts)
+
+        val monProc = List(
+            (mon(0)(monArgs: _*) :- true,
+                NoSync),
+            (mon(1)(resetMonArgs: _*) :-
+                (mon(0)(monArgs: _*), pSignal === True, qSignal === True),
+                NoSync),
+        )
+
+        val monProgressBlocks = List()
+
+        val assertion = false :- mon(1)(monArgs: _*)
 
         val globalVarNum = 1 + encoded.head.globalSignalTerms.size
         val system = SignalSystem(
